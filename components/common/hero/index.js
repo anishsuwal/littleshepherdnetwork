1ded29683da0b96d2bbd4f2750215423893e52fe
DELL <anishbhaktasuwal@gmail.com>
2022-07-05 18:29:54 +0545

    first commit

--- components/common/hero/index.js
+++ components/common/hero/index.js
@@ -14,11 +14,12 @@ export default function Hero() {
                         <header className=" group  py-10">
                             <h1 className="mb-1  text-4xl text-yellow-400 md:text-6xl">
                                 <br className="block md:hidden" />
-                                <span className="text-5xl font-bold">Little </span>
+                                <span className="text-5xl font-bold"> </span>
                                 <span className="inline-flex h-20 pt-2 overflow-x-hidden animate-type group-hover:animate-type-reverse 
                     whitespace-nowrap text-brand-accent will-change-transform text-5xl font-bold ">
-                                    Shepherd Network
+                                    Little Shepherd Network
                                 </span>
+                                
                                 <span
                                     className="box-border inline-block w-1 h-10 ml-2 -mb-2 bg-white md:-mb-4 md:h-16 animate-cursor will-change-transform"
                                 ></span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,22 +11,39 @@ module.exports = {
     extend: {
       animation: {
         cursor: 'cursor 2.6s linear infinite alternate',
-        type: 'type 2.8s ease-out .8s 1 normal both',
-        'type-reverse': 'type 2.8s ease-out 0s infinite alternate-reverse both',
+        type: 'type 5.8s ease-out .8s 1 normal both',
+        'type-reverse': 'type 5.8s ease-out 0s infinite alternate-reverse both',
       },
       keyframes: {
         type: {
           '0%': { width: '0ch' },
           '5%, 10%': { width: '1ch' },
-          '15%, 20%': { width: '2ch' },
-          '25%, 30%': { width: '3ch' },
-          '35%, 40%': { width: '4ch' },
-          '45%, 50%': { width: '5ch' },
-          '55%, 60%': { width: '6ch' },
-          '65%, 70%': { width: '7ch' },
-          '75%, 80%': { width: '8ch' },
-          '85%, 90%': { width: '9ch' },
-          '95%': { width: '10ch' },
+          '10%, 15%': { width: '2ch' },
+          '15%, 20%': { width: '3ch' },
+          '25%, 35%': { width: '4ch' },
+          '35%, 40%': { width: '5ch' },
+          '40%, 45%': { width: '6ch' },
+          '45%, 50%': { width: '7ch' },
+          '50%, 55%': { width: '8ch' },
+          '55%, 60%': { width: '9ch' },
+          '60%, 65%': { width: '10ch' },
+          '65%, 70%': { width: '11ch' },
+          '70%, 75%': { width: '12ch' },
+          '70%, 75%': { width: '13ch' },
+          '75%, 80%': { width: '14ch' },
+          '80%, 85%': { width: '15ch' },
+          '85%, 90%': { width: '16ch' },
+          '90%, 95%': { width: '17ch' },
+          '95%': { width: '18ch' },
+          '100%': { width: '19ch' },
+
+
+
+
+        
+
+   
+          
         },
       },
     },
